ffca982dc5584fa66b15b92bddd5354b16871b3e
sadratmh <sadrasadeghian8558@gmail.com>
2025-05-06 11:56:55 +0330

    Refactor import statement in fileUtils.ts to use updated path for Credentials type. This change enhances clarity and maintains consistency in type usage across the codebase.

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -1,7 +1,6 @@
 import fs from 'fs/promises';
 import path from 'path';
-import { Credentials } from './utils.js';
-
+import { Credentials } from './types';
 export const FILE_DIR_PREFIX = "src/files/";
 export const CREDENTIALS_FILE = "credentials.json";
 
